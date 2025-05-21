22b4eb2632803dbf0882d7fbf60393c86f17d08e
David Oliver <david@tenthirty.net>
2025-05-21 16:43:10 -0700

    moving things around

--- Sources/FreeTDSKit/SQLResult.swift
+++ Sources/FreeTDSKit/SQLResult.swift
@@ -8,13 +8,59 @@
 import Foundation
 
 public struct SQLResult {
-    public let columns: [String] // Column names
-    public let rows: [[String: SQLDataType]] // Rows with typed data
-    public let affectedRows: Int // Affected rows count
+    public let columns: [String]  // Column names
+    public let rows: [[String: SQLDataType]]  // Rows with typed data
+    public let affectedRows: Int  // Affected rows count
 
-    public init(columns: [String], rows: [[String: SQLDataType]], affectedRows: Int) {
+    public init(
+        columns: [String],
+        rows: [[String: SQLDataType]],
+        affectedRows: Int
+    ) {
         self.columns = columns
         self.rows = rows
         self.affectedRows = affectedRows
+    }
+
+}
+
+extension SQLResult {
+
+    public subscript(_ row: Int, _ column: String) -> SQLDataType? {
+        guard rows.indices.contains(row) else { return nil }
+        return rows[row][column]
+    }
+
+    /// Row index + column index (uses `columns`)
+    public subscript(_ row: Int, column colIndex: Int) -> SQLDataType? {
+        guard rows.indices.contains(row),
+            columns.indices.contains(colIndex)
+        else { return nil }
+        return rows[row][columns[colIndex]]
+    }
+}
+
+//public extension SQLResult {
+//
+//    /// Row index + column name → wrapped `Value`
+//    subscript(row rowIndex: Int, column columnName: String) -> Value? {
+//        guard rows.indices.contains(rowIndex),
+//              let raw = rows[rowIndex][columnName] else { return nil }
+//        return Value(column: columnName, raw: raw)
+//    }
+//
+//    /// Row index + column index → wrapped `Value`
+//    subscript(_ row: Int, column colIndex: Int) -> Value? {
+//        guard rows.indices.contains(row),
+//              columns.indices.contains(colIndex) else { return nil }
+//        let name = columns[colIndex]
+//        guard let raw = rows[row][name] else { return nil }
+//        return Value(column: name, raw: raw)
+//    }
+//}
+extension SQLResult: Sequence {
+    public typealias Element = [String: SQLDataType]
+    public func makeIterator() -> IndexingIterator<[[String: SQLDataType]]> {
+        rows.makeIterator()
     }
 }
